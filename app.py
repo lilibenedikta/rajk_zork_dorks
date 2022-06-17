42157bd21f56f83d09142c12348613db515be0d6
lilibenedikta <55950892+lilibenedikta@users.noreply.github.com>
2022-06-17 18:10:02 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#IMPORT asdasd
+
+from data_func import *
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+from dash.dependencies import Input, Output, State
+
+import chart_studio.plotly as py
+import plotly.graph_objs as go
+
+import pandas as pd
+import numpy as np
+
+import datetime
+
+
+
+#LAYOUT
+
+# external CSS stylesheets
+external_stylesheets = [
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    {
+        'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+        'rel': 'stylesheet',
+        'integrity': 'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
+        'crossorigin': 'anonymous'
+    }
+]
+
+app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
+
+app.title = 'Rajk Zork Kaland'
