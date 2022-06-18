1a1d7b8630bfc4c41a701fbac7e77df48b86cf5c
lilibenedikta <55950892+lilibenedikta@users.noreply.github.com>
2022-06-18 12:14:50 +0100

    css file + work on dash

--- app.py
+++ app.py
@@ -1,54 +1,47 @@
+import os
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output, State
+from flask import send_from_directory
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP, "style.css"])
 server = app.server
 
-colors = {
-    'background': '#000000',
-    'text': '#39FF14'
-}
 
-app.layout = html.Div([
-    'Rajk Zork Kaland',
-    dcc.Input(
-    placeholder='Enter a value...',
-    type='text',
-    value='',
-    style={
-            'width': '100%', 
-            'height': 200, 
-            'font-family':'Courier', 
-            'color':colors['text'],
-            'backgroundColor': colors['background']}
-    ),
-    dcc.Textarea(
-        id='textarea-state-example',
-        value='Textarea content initialized\nwith multiple lines of text',
-        style={
-            'width': '100%', 
-            'height': 200, 
-            'font-family':'Courier', 
-            'color':colors['text'],
-            'backgroundColor': colors['background']},
-    ),
-    html.Button('Submit', id='textarea-state-example-button', n_clicks=0),
-    html.Div(
-        id='textarea-state-example-output', 
-        style={'whiteSpace': 'pre-line',
-               'font-family':'Courier',
-               'color':colors['text'],
-               'backgroundColor': colors['background']})
-])
+@app.server.route("/assets/<path:path>")
+def static_file(path):
+    static_folder = os.path.join(os.getcwd(), "assets")
+    return send_from_directory(static_folder, path)
+
+
+colors = {"background": "#000000", "text": "#39FF14"}
+
+app.layout = html.Div(
+    [
+        html.Link(href="/assets/style.css", rel="stylesheet"),
+        html.P(id="paragraph-one", className="good-text"),
+        html.P(id="paragraph-two", className="good-text"),
+        dbc.Button("Submit", id="textarea-state-example-button", n_clicks=0),
+        dcc.Input(
+            placeholder="Enter a value...", type="text", id="text-input-small", value=""
+        ),
+        #dcc.Dropdown(["1", "2", "3"], "Choose", id="text-input-small"),
+    ]
+)
+
+intro = "Nagyokat pislogsz, fáj a fejed. Körbenézel. \n Ismerős minden, de valahogyan egy kicsit, egy kicsit más. \n"
+
 
 @app.callback(
-    Output('textarea-state-example-output', 'children'),
-    Input('textarea-state-example-button', 'n_clicks'),
-    State('textarea-state-example', 'value')
+    [Output("paragraph-one", "children"), Output("paragraph-two", "children")],
+    Input("textarea-state-example-button", "n_clicks"),
+    [State("text-input-small", "value"), State("paragraph-two", "children")],
 )
-def update_output(n_clicks, value):
+def update_output(n_clicks, value, last_para):
     if n_clicks > 0:
-        return 'You have entered: \n{}'.format(value)
+        return f"{last_para} and you chose {value}", "You have entered: \n{}".format(value)
+    return intro, last_para
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run_server(debug=True)
