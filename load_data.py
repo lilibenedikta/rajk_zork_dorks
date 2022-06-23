dc89751f58e670d6d4b18dbc67bc8c26e11111d3
lilibenedikta <55950892+lilibenedikta@users.noreply.github.com>
2022-06-23 23:51:29 +0100

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -5,13 +5,13 @@ import os
 from pathlib import Path
 
 
-ENDPOINT = "https://s3.eu-de.cloud-object-storage.appdomain.cloud"
+ENDPOINT = "s3.eu-de.cloud-object-storage.appdomain.cloud"
 S3 = boto3.client("s3", endpoint_url=ENDPOINT)
 BUCKET = os.environ["AWS_BUCKET"]
 
 
-def read_parquet_from_bucket(key: str) -> pd.DataFrame:
-    """Reads a parquet file from the given s3 bucket."""
+def read_pickle_from_bucket(key: str) -> pd.DataFrame:
+    """Reads a pickle file from the given s3 bucket."""
     obj = S3.get_object(Bucket=BUCKET, Key=key)
     dfp = Path("_.p")
     dfp.write_bytes(obj["Body"].read())
